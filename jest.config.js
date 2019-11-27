0434aa563f8febd3902c8eba0506b2016b2d76f6
Denner Stevens <denner.vidal@gmail.com>
2019-11-27 17:14:31 -0400

    Starting to fix test

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,9 @@ module.exports = {
   displayName: 'chat-tests',
   testMatch: ['**/__tests__/**/*.js'],
   testURL: 'http://localhost',
+  transformIgnorePatterns: ['/node_modules/(?!@tecsinapse/ui-kit).+\\.js$'],
   transform: {
     '^.+\\.js$': 'babel-jest',
   },
+  // automock: true, automock is causing errors on test, we should only mock react-mic dep
 };
