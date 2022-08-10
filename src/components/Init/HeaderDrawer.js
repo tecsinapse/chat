237a821b69dd30f27b585ec113112681cb37a543
Brenneguer <wbrenneguer07@gmail.com>
2022-08-10 13:33:41 -0300

    [#179604] Permitir que usuário sugira novos templates (Modelo de Mensagem) ajustar botão voltar para ir ao SendNotification

--- src/components/Init/HeaderDrawer.js
+++ src/components/Init/HeaderDrawer.js
@@ -26,6 +26,14 @@ export const HeaderDrawer = ({
 }) => {
   const innerClasses = useStyles();
 
+  const handleBackButton = () => {
+    setView(
+      view === COMPONENT_LOCATION.MESSAGE_SUGESTION
+        ? COMPONENT_LOCATION.SEND_NOTIFICATION
+        : homeLocation
+    );
+  };
+
   return (
     <div className={classes.drawerHeader}>
       <Grid container justify="space-between">
@@ -44,7 +52,7 @@ export const HeaderDrawer = ({
                   }}
                 >
                   <Icon
-                    onClick={() => setView(homeLocation)}
+                    onClick={handleBackButton}
                     color={theme.palette.primary.main}
                     size={1.25}
                     className={innerClasses.backIconStyles}
--- src/components/MessageSugestion/MessageSugestion.js
+++ src/components/MessageSugestion/MessageSugestion.js
@@ -2,13 +2,13 @@ import React from "react";
 
 export function MessageSugestion({ keycloakId, connectionKey }) {
   const url = `${process.env.REACT_APP_MESSAGE_SUGESTION_URL}?kcid=${keycloakId}&connectionkey=${connectionKey}`;
-  const styleProps = "&alignCenter=1&hideTitle=1&transparentBackground=1";
+  const styleProps = "&alignCenter=1&transparentBackground=1";
 
   return (
     <iframe
       src={`${url}${styleProps}`}
       width="100%"
-      height="640px"
+      height="850px"
       frameBorder="0"
       marginHeight="0"
       title="Novo Modelo de Mensagem"
