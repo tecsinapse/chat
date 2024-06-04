e928d078c39a33a8331f43e244f16dda11989d45
Gabriel Butka <gabriel.butka@tecsinapse.com.br>
2024-06-04 10:23:04 -0300

    [TS-6697] [QA] Menu de dropdown da mídia de video está abrindo no local errado

--- src/components/Chat/Maximized/MessageView/Message/VideoLoader/VideoLoader.js
+++ src/components/Chat/Maximized/MessageView/Message/VideoLoader/VideoLoader.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const VideoLoader = ({ media, classes }) => (
-  <video controls width={200} className={classes.thumbnail}>
+  <video width={200} className={classes.thumbnail} controlsList='nodownload noplaybackrate' disablePictureInPicture controls>
     <source src={media.url} />
     {/* TODO: ADD A REAL TRACK OBJECT */}
     <track default kind="captions" src={media.url} />
